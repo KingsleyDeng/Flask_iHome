bfd01ca7fdcfbf372059cb2d71fc707ffdc5fe49
KingsleyDeng <460556933@qq.com>
2020-05-17 20:36:48 +0800

    iHome 保存房屋前端信息

--- ihome/static/js/ihome/newhouse.js
+++ ihome/static/js/ihome/newhouse.js
@@ -3,11 +3,11 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     // 向后端去获取城区信息
     $.get("/api/v1.0/areas", function (resp) {
 
-        if (resp.errno == "0"){
+        if (resp.errno == "0") {
             var areas = resp.data;
             // for (i = 0; i < areas.length; i++){
             //     var area = areas[i]
@@ -15,7 +15,7 @@ $(document).ready(function(){
             // }
 
             // 使用js模板
-            var html = template("areas-tmpl",{areas: areas});
+            var html = template("areas-tmpl", {areas: areas});
             $("#area-id").html(html);
 
         } else {
@@ -23,6 +23,54 @@ $(document).ready(function(){
         }
 
     }, "json")
+
+    $("#form-house-info").submit(function (e) {
+        e.preventDefault()
+
+        // 处理表单数据
+        var data = {};
+        $("#form-house-info").serializeArray().map(function (x) {
+            data[x.name] = x.value
+        });
+
+        // 处理设置id信息
+        var facility = [];
+        $(":checked[name=facility]").each(function (index, x) {
+            facility[index] = $(x).val()
+        });
+
+        data.facility = facility;
+
+        // 向后端发送请求
+        $.ajax({
+            url : "/api/v1.0/houses/info",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            dataType:"json",
+            headers:{
+                "X-CSRFToken":getCookie("csrf_token")
+            },
+            success:function (resp) {
+                if (resp.errno=="4101"){
+                    //    用户未登录
+                    location.href = "/login.html";
+                } else if (resp.errno=="0"){
+                    // 隐藏表单信息
+                    $("#form-house-info").hide();
+                    // 显示图片表单
+                    $("#form-house-image").show();
+                    // 设置图片表单的house_id
+                    $("#house-id").val(resp.data.house_id);
+                } else {
+                    alert(resp.errmsg);
+                }
+            }
+            
+        })
+
+
+    })
 
 })
 
